300497c737c459886c6638cd7a44e7a631b994a1
Luis Frontanilla <lrdolucho@gmail.com>
2022-01-20 00:53:42 -0400

    Updated dependency versions, using lwjgl3

--- desktop/src/com/epicness/alejandria/desktop/DesktopLauncher.java
+++ desktop/src/com/epicness/alejandria/desktop/DesktopLauncher.java
@@ -1,18 +1,16 @@
 package com.epicness.alejandria.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.epicness.alejandria.AlejandriaApp;
-
 import static com.epicness.alejandria.Constants.INITIAL_WINDOW_SIZE;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.epicness.alejandria.AlejandriaApp;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = INITIAL_WINDOW_SIZE;
-        config.height = INITIAL_WINDOW_SIZE;
-        config.forceExit = false;
-        new LwjglApplication(new AlejandriaApp(), config);
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setWindowedMode(INITIAL_WINDOW_SIZE, INITIAL_WINDOW_SIZE);
+        new Lwjgl3Application(new AlejandriaApp(), config);
         // See package com.epicness.alejandria.standalone for demos
     }
 }
