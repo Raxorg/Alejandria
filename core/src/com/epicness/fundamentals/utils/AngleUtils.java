74bb7b55db4ecc46522aef65cd67814f41c58e35
Luis Frontanilla <lrdolucho@gmail.com>
2022-02-02 21:24:22 -0400

    Added circular clamp to angle utils

--- core/src/com/epicness/fundamentals/utils/AngleUtils.java
+++ core/src/com/epicness/fundamentals/utils/AngleUtils.java
@@ -5,4 +5,10 @@ public class AngleUtils {
     public static float normalize(float angle) {
         return (angle % 360f) + (angle < 0f ? 360f : 0f);
     }
+
+    public static float circularClamp(float angle, float min, float max) {
+        float diff = Math.abs(min - max) / 2f;
+        angle = angle < min ? (angle + diff < min ? max : min) : (angle > max ? (angle - diff > max ? min : max) : angle);
+        return angle;
+    }
 }
