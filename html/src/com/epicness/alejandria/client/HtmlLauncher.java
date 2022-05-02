959ede7da791986d7f2562462811eef68c1280dc
Luis Frontanilla <lrdolucho@gmail.com>
2022-05-02 17:59:57 -0400

    Set html window size

--- html/src/com/epicness/alejandria/client/HtmlLauncher.java
+++ html/src/com/epicness/alejandria/client/HtmlLauncher.java
@@ -7,16 +7,16 @@ import com.epicness.alejandria.AlejandriaApp;
 
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                // Resizable application, uses available space in browser
-                return new GwtApplicationConfiguration(true);
-                // Fixed size application:
-                //return new GwtApplicationConfiguration(480, 320);
-        }
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        // Resizable application, uses available space in browser
+        return new GwtApplicationConfiguration(800, 800, true);
+        // Fixed size application:
+        //return new GwtApplicationConfiguration(480, 320);
+    }
 
-        @Override
-        public ApplicationListener createApplicationListener () {
-                return new AlejandriaApp();
-        }
+    @Override
+    public ApplicationListener createApplicationListener() {
+        return new AlejandriaApp();
+    }
 }
