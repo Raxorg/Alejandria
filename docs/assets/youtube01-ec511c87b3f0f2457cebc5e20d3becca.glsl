c4812f8e59aa363e8ce10fbe670bbc6e84b972d5
Raxorg <lrdolucho@gmail.com>
2023-07-11 17:22:26 -0400

    New web build for GitHub pages

---
+++ docs/assets/youtube01-ec511c87b3f0f2457cebc5e20d3becca.glsl
@@ -0,0 +1,26 @@
+#ifdef GL_ES
+#define LOWP lowp
+precision mediump float;
+#else
+#define LOWP
+#endif
+varying LOWP vec4 v_color;
+varying vec2 v_texCoord0;
+
+uniform sampler2D u_texture;
+uniform vec2 u_resolution;
+uniform float time;
+
+void main() {
+    vec2 pos = gl_FragCoord.xy;
+    vec2 reso = u_resolution;
+    vec2 uv = (pos * 2.0 - u_resolution) / u_resolution.y;
+
+    float d = length(uv);
+    d = sin(d * 8.0 + time) / 8.0;
+    d = abs(d);
+    d = smoothstep(0.0, 0.1, d);
+
+    // Result
+    gl_FragColor = vec4(d, d, d, 1.0);
+}
