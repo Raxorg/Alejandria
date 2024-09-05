ef25570478d0094129200b4ccbb99680cd7d63b2
Raxorg <lrdolucho@gmail.com>
2024-09-05 19:32:01 -0400

    Sounds now play properly on the Sound Pitch module

--- core/src/main/java/com/epicness/alejandria/showcase/modules/audio/SoundPitchDrawable.java
+++ core/src/main/java/com/epicness/alejandria/showcase/modules/audio/SoundPitchDrawable.java
@@ -1,10 +1,13 @@
 package com.epicness.alejandria.showcase.modules.audio;
 
 import static com.badlogic.gdx.graphics.Color.RED;
+import static com.epicness.alejandria.showcase.constants.AudioConstants.SOUNDBOARD_CELL_SIZE;
+import static com.epicness.alejandria.showcase.constants.AudioConstants.SOUNDBOARD_CIRCLE_RADIUS;
+import static com.epicness.alejandria.showcase.constants.AudioConstants.SOUNDBOARD_COLS;
+import static com.epicness.alejandria.showcase.constants.AudioConstants.SOUNDBOARD_ROWS;
 import static com.epicness.alejandria.showcase.constants.ShowcaseConstants.SHOWCASE_STRIPE_HEIGHT;
-import static com.epicness.alejandria.showcase.constants.ShowcaseConstants.SHOWCASE_TOP;
 import static com.epicness.fundamentals.constants.ColorConstants.LIGHT_DIRT;
-import static com.epicness.fundamentals.constants.SharedConstants.CAMERA_HALF_WIDTH;
+import static com.epicness.fundamentals.constants.SharedConstants.VIEWPORT_HALF_WIDTH;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -13,31 +16,35 @@ import com.epicness.fundamentals.renderer.ShapeDrawerPlus;
 import com.epicness.fundamentals.renderer.ShapeRendererPlus;
 import com.epicness.fundamentals.stuff.grid.Cell;
 import com.epicness.fundamentals.stuff.grid.DefaultCellGrid;
-import com.epicness.fundamentals.stuff.shapes.bidimensional.Line;
+import com.epicness.fundamentals.stuff.shapes.bidimensional.Circle;
 
 public class SoundPitchDrawable implements ModuleDrawable {
 
     private final DefaultCellGrid grid;
-    private final Line line;
+    private final Circle[] circles;
 
     public SoundPitchDrawable(Sprite cell) {
-        grid = new DefaultCellGrid(cell, 8, 5);
-        grid.setCellSize(120f);
-        grid.translate(CAMERA_HALF_WIDTH - grid.getWidth() / 2f, SHOWCASE_STRIPE_HEIGHT * 2f);
+        grid = new DefaultCellGrid(cell, SOUNDBOARD_COLS, SOUNDBOARD_ROWS);
+        grid.setCellSize(SOUNDBOARD_CELL_SIZE);
+        grid.translate(VIEWPORT_HALF_WIDTH - grid.getWidth() / 2f, SHOWCASE_STRIPE_HEIGHT * 2f);
         grid.setColor(LIGHT_DIRT);
 
-        line = new Line(
-            grid.cells[0][0].getX(), SHOWCASE_STRIPE_HEIGHT,
-            grid.cells[0][0].getX(), SHOWCASE_TOP, 5f,
-            RED
-        );
+        circles = new Circle[SOUNDBOARD_ROWS];
+        for (int i = 0; i < circles.length; i++) {
+            circles[i] = new Circle(
+                grid.cells[0][i].getX(), grid.cells[0][i].getCenterY() - SOUNDBOARD_CIRCLE_RADIUS,
+                SOUNDBOARD_CIRCLE_RADIUS, RED
+            );
+        }
     }
 
     @Override
     public void draw(SpriteBatch spriteBatch, ShapeDrawerPlus shapeDrawer, ShapeRendererPlus shapeRenderer) {
         spriteBatch.begin();
         grid.draw(spriteBatch);
-        line.draw(shapeDrawer);
+        for (int i = 0; i < circles.length; i++) {
+            circles[i].draw(shapeDrawer);
+        }
         spriteBatch.end();
     }
 
@@ -50,7 +57,7 @@ public class SoundPitchDrawable implements ModuleDrawable {
         return grid.cells;
     }
 
-    public Line getLine() {
-        return line;
+    public Circle[] getCircles() {
+        return circles;
     }
 }
